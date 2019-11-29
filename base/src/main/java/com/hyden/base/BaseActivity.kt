703dd223a5b5a7e3950c6e00754601242612ccc4
hyojin <jin33032@gmail.com>
2019-11-30 01:59:10 +0900

    UPDATE [ base 수정 - 뷰바인딩처리 ]

--- base/src/main/java/com/hyden/base/BaseActivity.kt
+++ base/src/main/java/com/hyden/base/BaseActivity.kt
@@ -2,13 +2,31 @@ package com.hyden.base
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import io.reactivex.disposables.CompositeDisposable
 
 abstract class BaseActivity<B : ViewDataBinding>(private val layoutId : Int) : AppCompatActivity() {
 
     lateinit var binding : B
+    lateinit var compositeDisposable: CompositeDisposable
+
+    abstract fun initBind()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        compositeDisposable = CompositeDisposable()
+
+        binding = DataBindingUtil.inflate(layoutInflater, layoutId, null, false)
+        setContentView(binding.root)
+        binding.lifecycleOwner = this
+
+        initBind()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
--- base/src/main/java/com/hyden/base/BaseFragment.kt
+++ base/src/main/java/com/hyden/base/BaseFragment.kt
@@ -1,15 +1,40 @@
 package com.hyden.base
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import com.hyden.util.LogUtil.logE
+import io.reactivex.disposables.CompositeDisposable
 
 abstract class BaseFragment<B : ViewDataBinding>(private val layoutId : Int) : Fragment() {
 
     lateinit var binding : B
+    lateinit var compositeDisposable: CompositeDisposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        compositeDisposable = CompositeDisposable()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        binding = DataBindingUtil.inflate(inflater,layoutId,container,false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        compositeDisposable.clear()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
