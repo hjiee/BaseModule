7033b2d79652d4a1e074c7f8fc5bbfa6d2aecece
hyojin <jin33032@gmail.com>
2019-11-30 01:59:59 +0900

    ADD [ RecyclerView Adapter 추가 ]

--- base/src/main/java/com/hyden/ext/RecyclerViewExt.kt
+++ base/src/main/java/com/hyden/ext/RecyclerViewExt.kt
@@ -1,2 +1,9 @@
 package com.hyden.ext
 
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+@BindingAdapter(value = ["bindItems"])
+fun RecyclerView.bindItems() {
+
+}
