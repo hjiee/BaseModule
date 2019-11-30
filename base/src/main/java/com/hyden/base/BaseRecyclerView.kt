3e072728d9f6b64dac9ac7da9f7d5149bca612de
hyojin <jin33032@gmail.com>
2019-11-30 20:16:09 +0900

    UPDATE [ Recyclerview DiffUtil로 기능 개선 ]

--- base/src/main/java/com/hyden/base/BaseRecyclerView.kt
+++ base/src/main/java/com/hyden/base/BaseRecyclerView.kt
@@ -1,39 +1,53 @@
 package com.hyden.base
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.hyden.util.RecyclerDiffUtil
 
 class BaseRecyclerView {
-    abstract class Adapter<ITEM : Any, B : ViewDataBinding>(
+    abstract class Adapter<ITEM : Any, B : ViewDataBinding, T>(
         private val layoutId: Int,
-        private val bindingVariableId: Int?
+        private val bindingVariableId: Int?,
+        private val event : ((T) -> Unit)? = null
     ) : RecyclerView.Adapter<ViewHolder<B>>() {
 
 
         private var list = listOf<ITEM>()
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder<B> =
-            object : ViewHolder<B>(
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder<B> {
+            val holder = object : ViewHolder<B>(
                 layoutId,
                 parent,
                 bindingVariableId
-            ) {
+            ) {}
+            holder.itemView.setOnClickListener {
+                event?.invoke(list[holder.adapterPosition] as T)
             }
-
+            return holder
+        }
 
         override fun getItemCount(): Int = list.size
-
 
         override fun onBindViewHolder(holder: ViewHolder<B>, position: Int) =
             holder.onBind(list[position])
 
         fun replaceAll(items : List<ITEM>) {
-
             list = items
             notifyDataSetChanged()
+        }
+
+        fun updateItems(items : List<ITEM>) {
+            RecyclerDiffUtil(list,items).apply {
+                list = items
+                DiffUtil.calculateDiff(this).let {
+                    it.dispatchUpdatesTo(this@Adapter)
+                }
+            }
         }
 
     }
@@ -47,6 +61,7 @@ class BaseRecyclerView {
     ) {
 
         val binding = DataBindingUtil.bind<B>(itemView)
+
 
         fun onBind(item: Any?) {
             bindingVariableId?.let {
--- base/src/main/java/com/hyden/ext/RecyclerViewExt.kt
+++ base/src/main/java/com/hyden/ext/RecyclerViewExt.kt
@@ -2,8 +2,15 @@ package com.hyden.ext
 
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.hyden.base.BaseRecyclerView
+import java.util.Collections.replaceAll
 
 @BindingAdapter(value = ["bindItems"])
-fun RecyclerView.bindItems() {
-
+fun RecyclerView.bindItems(items : List<Any>?) {
+    items?.let {
+        (adapter as? BaseRecyclerView.Adapter<Any,*, Any?>)?.run {
+//            replaceAll(items)
+            updateItems(items)
+        }
+    }
 }
