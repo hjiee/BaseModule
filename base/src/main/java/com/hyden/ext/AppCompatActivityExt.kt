688ad1d2e3f4ffa5c55d00ea034997001d91dd12
hyojin <jin33032@gmail.com>
2019-12-02 21:32:10 +0900

    ADD [ 화면 전환 애니메이션 추가 ]

--- base/src/main/java/com/hyden/ext/AppCompatActivityExt.kt
+++ base/src/main/java/com/hyden/ext/AppCompatActivityExt.kt
@@ -2,9 +2,11 @@ package com.hyden.ext
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.hyden.base.R
 
 fun AppCompatActivity.replaceFragment(fragment : Fragment, layoutId : Int) {
     supportFragmentManager.beginTransaction()
+        .setCustomAnimations(R.anim.fade_in,R.anim.fade_out)
         .replace(layoutId,fragment)
         .commitNow()
 }
--- base/src/main/java/com/hyden/ext/ImageViewExt.kt
+++ base/src/main/java/com/hyden/ext/ImageViewExt.kt
@@ -10,6 +10,7 @@ fun ImageView.loadUrl(url: String?) {
     url?.let {
         Glide.with(this)
             .load(it)
+            .override(450,650)
             .apply(RequestOptions.fitCenterTransform().centerCrop())
             .into(this)
     }
