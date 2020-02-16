84ceeed4a83d83ea0b99c8821a0712a5e28e62e5
hyojin <jin33032@gmail.com>
2020-02-16 19:49:37 +0900

    UPDATE [ date 포멧 변경 ]

--- base/src/main/java/com/hyden/ext/TextViewExt.kt
+++ base/src/main/java/com/hyden/ext/TextViewExt.kt
@@ -2,14 +2,31 @@ package com.hyden.ext
 
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.hyden.util.ConstValueUtil.Companion.YYMMDDHHMMSS_FORMAT
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.time.milliseconds
 
 @BindingAdapter(value = ["dateFormat"], requireAll = false)
 fun TextView.dateFormat(date : Date) {
-    val sdf = SimpleDateFormat("yyyy.MM.dd HH:mm:ss")
+    val sdf = SimpleDateFormat(YYMMDDHHMMSS_FORMAT)
     val formDate = sdf.format(date).toString()
     text = formDate
+}
+
+@BindingAdapter(value = ["elapsedTimeFormat"], requireAll = false)
+fun TextView.elapsedTimeFormat(createdDate : Date) {
+    val sdf = SimpleDateFormat(YYMMDDHHMMSS_FORMAT)
+    val formDate = sdf.format(createdDate).toString()
+
+    // 현재 시간 ( milliseconds )
+    val currentTime = Date().time
+    // 생성 시간 ( milliseconds )
+    val createdTime = SimpleDateFormat(YYMMDDHHMMSS_FORMAT).parse(formDate).time
+
+    val elapsedTime = currentTime - createdTime
+
+    text = elapsedTime.elapsedTimeFormatter(createdDate)
 }
 
 @BindingAdapter(value = ["likeCount"], requireAll = false)
--- base/src/main/java/com/hyden/util/ConstValueUtil.kt
+++ base/src/main/java/com/hyden/util/ConstValueUtil.kt
@@ -4,5 +4,7 @@ class ConstValueUtil {
     companion object {
         const val THROTTLE = 500L // 클릭 딜레이 시간
         const val ITEM_DECORATION = 10
+        const val YYMMDDHHMMSS_FORMAT = "yyyy.MM.dd HH:mm:ss"
+        const val YYMMDD_FORMAT = "yyyy.MM.dd"
     }
 }
